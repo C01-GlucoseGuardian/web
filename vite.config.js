813ccfa146a4e6c2b4327e3d3fb7ec76c2799e71
handymenny <handymenny@outlook.com>
2023-02-05 23:04:10 +0100

    add entrypoints to viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,22 @@ const path = require('path')
 export default {
   root: path.resolve(__dirname, 'src'),
   base: "/<repo>/",
+  build: {
+    outDir: path.join(__dirname, "src/dist"),
+    rollupOptions: {
+      input: {
+        main:  path.resolve(__dirname, 'src/index.html'),
+        error: path.resolve(__dirname, 'src/error/genericerror.html'),
+        login: path.resolve(__dirname, 'src/login/login.html'),
+        loginAdmin: path.resolve(__dirname, 'src/login/contactanadmin.html'),
+        newprofilopaziente: path.resolve(__dirname, 'src/creazioneprofili/newprofilopaziente.html'),
+        newtutore: path.resolve(__dirname, 'src/creazioneprofili/newtutore.html'),
+        temp: path.resolve(__dirname, 'src/creazioneprofili/temp.html'),
+        patientlist: path.resolve(__dirname, 'src/dataview/patientslist.html'),
+        signup: path.resolve(__dirname, 'src/signup/signup.html'),
+      },
+    },
+  },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
